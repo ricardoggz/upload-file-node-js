e0b1ef2ccaa6afcaf71b31d78beeb57cfa0dced2
ricardoggz <ricardo_ggzicm@outlook.com>
2023-08-23 10:09:50 -0600

    first version

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const path= require('path')
+const app = express()
+
+app.use(fileUpload())
+app.use('/files', express.static(path.join(__dirname, 'files')))
+app.get('/', (req, res)=> res.send('Home'))
+app.post('/upload',(req,res) => {
+
+    let EDFile = req.files.file
+
+    EDFile.mv(`./files/${EDFile.name}`,err => {
+
+        if(err) return res.status(500).send({ message : err })
+
+
+        return res.status(200).send({ message : 'File upload' })
+
+    })
+
+})
+app.listen(4000, ()=>console.log('Server running'))
