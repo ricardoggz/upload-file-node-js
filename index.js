d525fbdc7af8345616286e35c91e6e4bea0c19d2
ricardoggz <ricardo_ggzicm@outlook.com>
2023-08-23 20:52:02 -0600

    [add]: new code

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-const express = require('express')
-const fileUpload = require('express-fileupload')
-const path= require('path')
+const express = require('express');
+const fileUpload = require('express-fileupload');
 const cors = require('cors')
-const app = express()
+const app = express();
+const port = 3001;
 
-app.use(fileUpload())
+// Habilitar express-fileupload
+app.use(fileUpload());
 app.use(cors())
-app.use('/files', express.static(path.join(__dirname, 'files')))
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname + '/public/index.html'))
-})
-app.get('/', (req, res)=> res.send('Home'))
-app.post('/upload',(req,res) => {
+// Ruta para manejar la carga de la imagen
+app.post('/upload', (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No se encontraron archivos para cargar.');
+  }
 
-    let EDFile = req.files.student_license
+  const image = req.files.image;
 
-    EDFile.mv(`./files/${EDFile.name}`,err => {
+  // Mover la imagen a la carpeta de destino
+  image.mv(__dirname + '/files/' + image.name, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
 
-        if(err) return res.status(500).send({ message : err })
+    res.send('Imagen cargada exitosamente');
+  });
+});
 
-
-        return res.status(200).send({ message : 'File upload' })
-
-    })
-
-})
-app.listen(4000, ()=>console.log('Server running'))
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
